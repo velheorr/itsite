dce1dde606a564f544e76d2732f068c5c95cee16
Velheorr <velheorr@gmail.com>
2025-12-19 11:32:15 +0500

    ++

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Outlet} from "react-router";
-import page from "./assets/img/page.svg";
+
 import './css.scss'
 
 const RootLayout = () => {
     return (
-        /*<main style={{backgroundImage: `url(${page})`}} className={'bg'}>*/
+
         <main  className={'bg'}>
             <Outlet/>
         </main>
